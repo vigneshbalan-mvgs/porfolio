bb4943ed2a4bbec16a1fa1ef08dbfb4632f2a708
vigneshbalan-mvgs <vigneshbalanmvgs2003@gmail.com>
2023-12-12 08:32:47 +0530

    0.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        hello world 
+        <br>
+        </br>
+        its mvgs here
+        im currenly working on learning and testing apis and react components
+        that what im corrently working on .
+        <br></br>
+        its my honored to show you that how it accutally working on my phone and pc at the same time its helpful to create a new and nice web app that will perfectly work on both pc and mobile without any problems 
       </header>
     </div>
   );
